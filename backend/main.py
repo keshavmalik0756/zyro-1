8adf3f68b484158fd8557569ac17891ee3d1eaa4
kanha_workspace <kanhaiya.gupta@ez.works>
2026-01-10 19:24:46 +0530

    updated main.py

--- backend/main.py
+++ backend/main.py
@@ -45,7 +45,8 @@ app.add_middleware(
         "https://zyro-67cv9686v-pranshus-projects-daa60a75.vercel.app",
         "https://zyro-2dox.vercel.app",
         "https://zyro-bd7p9xa4c-pranshus-projects-daa60a75.vercel.app",
-        "https://zyro-2dox.vercel.app"
+        "https://zyro-2dox.vercel.app",
+        "https://zyro-forked.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
